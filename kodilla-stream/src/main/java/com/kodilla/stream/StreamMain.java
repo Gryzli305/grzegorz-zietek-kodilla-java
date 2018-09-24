145b417a695ad6db899d8cb49d958352a9d90353
unknown <gzietek@tlen.pl>
2018-09-24 23:50:09 +0200

    Strim  first steps

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,19 +3,27 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
-
+import com.kodilla.stream.iterate.NumbersGenerator;
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println("decorating text with lamda \n");
-        poemBeautifier.beautify("test", (tekst -> "---===" + tekst + "===---"));
-        poemBeautifier.beautify("Loading", (tekst -> "|/-\\|*" + tekst + "*|/-\\|"));
-        poemBeautifier.beautify("test", (tekst -> "-=-=-=" + tekst + "=-=-=-"));
-        poemBeautifier.beautify("Java Master", (tekst -> "******" + tekst.toUpperCase() + "******"));
-        poemBeautifier.beautify("TO LOWER CASE", (tekst -> "BBAA " + tekst.toLowerCase() + " AABB"));
-        poemBeautifier.beautify("TO LOWER CASE", (tekst -> "******" + tekst.hashCode() + "******"));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
 }
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        System.out.println("decorating text with lamda \n");
+//        poemBeautifier.beautify("test", (tekst -> "---===" + tekst + "===---"));
+//        poemBeautifier.beautify("Loading", (tekst -> "|/-\\|*" + tekst + "*|/-\\|"));
+//        poemBeautifier.beautify("test", (tekst -> "-=-=-=" + tekst + "=-=-=-"));
+//        poemBeautifier.beautify("Java Master", (tekst -> "******" + tekst.toUpperCase() + "******"));
+//        poemBeautifier.beautify("TO LOWER CASE", (tekst -> "BBAA " + tekst.toLowerCase() + " AABB"));
+//        poemBeautifier.beautify("TO LOWER CASE", (tekst -> "******" + tekst.hashCode() + "******"));
+//    }
+//}
+
+
+
 //        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 //
 //        System.out.println("Calculating expressions with lambdas");
