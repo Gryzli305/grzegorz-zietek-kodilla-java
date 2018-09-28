d71e3a7fc3d904fc68acb9de2fcb9cebb56e9e42
unknown <gzietek@tlen.pl>
2018-09-28 18:29:13 +0200

    poprawiona od 1 do 7

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -8,16 +8,16 @@ public final class Forum {
     private final List<ForumUser> theForumUser = new ArrayList<>();
 
     public Forum() {
-        theForumUser.add(new ForumUser(00,"Bender", 'M', LocalDate.of(2996,5,1),100));
-        theForumUser.add(new ForumUser(01,"Fry", 'M', LocalDate.of(1985,11,8),20));
-        theForumUser.add(new ForumUser(02,"Leela", 'F', LocalDate.of(2002,6,18),0));
-        theForumUser.add(new ForumUser(03,"Amy", 'F', LocalDate.of(2008,3,9),0));
-        theForumUser.add(new ForumUser(04,"Tim", 'M', LocalDate.of(2004,12,26),0));
-        theForumUser.add(new ForumUser(05,"Loli", 'F', LocalDate.of(1980,1,12),1));
-        theForumUser.add(new ForumUser(06,"Farnsworth", 'M', LocalDate.of(1960,7,14),5));
-        theForumUser.add(new ForumUser(07,"Ryan", 'M', LocalDate.of(1986,9,30),1));
+        theForumUser.add(new ForumUser(0,"Bender", 'M', LocalDate.of(2996,5,1),100));
+        theForumUser.add(new ForumUser(1,"Fry", 'M', LocalDate.of(1985,11,8),20));
+        theForumUser.add(new ForumUser(2,"Leela", 'F', LocalDate.of(2002,6,18),0));
+        theForumUser.add(new ForumUser(3,"Amy", 'F', LocalDate.of(2008,3,9),0));
+        theForumUser.add(new ForumUser(4,"Tim", 'M', LocalDate.of(2004,12,26),0));
+        theForumUser.add(new ForumUser(5,"Loli", 'F', LocalDate.of(1980,1,12),1));
+        theForumUser.add(new ForumUser(6,"Farnsworth", 'M', LocalDate.of(1960,7,14),5));
+        theForumUser.add(new ForumUser(7,"Ryan", 'M', LocalDate.of(1986,9,30),1));
 
-        /* nie rozumiem dlaczego warość 08 wykracza poza Integer, a np 10 już jest ok?
+        /* nie rozumiem dlaczego wartość 08 wykracza poza Integer, a np 10 już jest ok?
         theForumUser.add(new ForumUser(08,"Bender", 'M', LocalDate.of(2000,5,3),1));
         */
     }
