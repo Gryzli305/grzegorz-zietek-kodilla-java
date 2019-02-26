a86250b93086cf46b2cce8f15cd4896998b4352b
unknown <gzietek@tlen.pl>
2019-02-26 20:44:00 +0100

    Selenium Webdriver

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+
 public class StoredProcTestSuite {
     @Test
     public void testUpdateVipLevels() throws SQLException {
@@ -24,38 +24,38 @@ public class StoredProcTestSuite {
         statement.executeUpdate(sqlUpdate);
 
         //When
-//        String sqlProcedureCall = "CALL UpdateVipLevels()";
-//        statement.execute(sqlProcedureCall);
-//
-//        //Then
-//        String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM readers WHERE vip_level=\"Not set\"";
-//        ResultSet rs = statement.executeQuery(sqlCheckTable);
-//        int howMany = -1;
-//        if (rs.next()) {
-//            howMany = rs.getInt("how_many");
-//        }
-//        assertEquals(0, howMany);
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM readers WHERE vip_level=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("how_many");
+        }
+        assertEquals(0, howMany);
     }
 
-//    @Test
-//    public void testUpdateBestsellers() throws SQLException {
-//        //Given
-//        DbManager dbManager = DbManager.getInstance();
-//        String sqlUpdate = "UPDATE books SET bestseller=null";
-//        Statement statement = dbManager.getConnection().createStatement();
-//        statement.executeUpdate(sqlUpdate);
-//
-//        //When
-//        String sqlProcedureCall = "CALL UpdateBestsellers()";
-//        statement.execute(sqlProcedureCall);
-//
-//        //Then
-//        String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM books WHERE bestseller is null";
-//        ResultSet rs = statement.executeQuery(sqlCheckTable);
-//        int howMany = -1;
-//        if (rs.next()) {
-//            howMany = rs.getInt("how_many");
-//        }
-//        assertEquals(0, howMany);
-//    }
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE books SET bestseller=null";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM books WHERE bestseller is null";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("how_many");
+        }
+        assertEquals(0, howMany);
+    }
 }
