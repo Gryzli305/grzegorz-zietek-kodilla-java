1e1cd373df0995a2c9ccb737bd7708630445a868
unknown <gzietek@tlen.pl>
2019-02-27 19:12:57 +0100

    selenium v2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,15 +1,17 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+
 public class EBayTestingApp {
-//
-//    public final static String SEARCHFIELD = "_nkw";
-//
-//    public static void main(String[] args) {
-//        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-//        driver.get("https://www.ebay.com/");
-//
-//        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
-//        searchField.sendKeys("Laptop");
-//        searchField.submit();
-//    }
+
+    public final static String SEARCHFIELD = "_nkw";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,16 +1,18 @@
 package com.kodilla.testing2.google;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+
 public class GoogleTestingApp {
-//    public static final String SEARCHFIELD = "gsr";
-//
-//    public static void main(String[] args) {
-//
-//
-//        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-//        driver.get("https://www.google.com");
-//
-//        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
-//        searchField.sendKeys("Kodilla");
-//        searchField.submit();
-//    }
+    public static final String SEARCHFIELD = "gsr";
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
+
+        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
 }
